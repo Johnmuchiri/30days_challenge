8f3bc5c920e82ffc9b9d6216789c0eb2b6ccb535
john mwangi <mjohnmwangi@gmail.com>
2020-04-27 20:33:04 +0300

    added time and space complexity

--- day2/happy_number.py
+++ day2/happy_number.py
@@ -1,5 +1,24 @@
 import unittest
 
+"""
+
+A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+
+Return True if n is a happy number, and False if not.
+
+Example: 
+
+Input: 19
+Output: true
+Explanation: 
+12 + 92 = 82
+82 + 22 = 68
+62 + 82 = 100
+12 + 02 + 02 = 1
+
+Approach 1 time complexity O(loggn) and space complexity of O(logn)
+"""
+
 
 class Solution:
     def isHappy(self, n: int) -> bool:
@@ -13,7 +32,6 @@ class Solution:
                 break
             visited.append(happy_number)
             happy_number = self.get_next(happy_number)
-
         return result
 
     def get_next(self, n):
